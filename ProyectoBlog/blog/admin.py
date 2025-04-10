dd3e49b4950a07ed6b87c2af185aae349b3254db
EnriqueSazoR <luisrosales9598@gmail.com>
2025-04-09 21:16:16 -0600

    Se crearon modelos (Categoria y Publicacion), se creo un superusuario para la administración de la base de datos y se realizaron las migraciones a la bd

--- ProyectoBlog/blog/admin.py
+++ ProyectoBlog/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.models import Categoria, Publicacion
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Publicacion)
--- ProyectoBlog/blog/models.py
+++ ProyectoBlog/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# Modelo Categoría
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+
+
+# Modelo Publicaciones
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.TextField()
+    Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.titulo
+    
